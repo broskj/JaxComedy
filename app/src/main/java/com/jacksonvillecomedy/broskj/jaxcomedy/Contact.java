a8b00d6b6c7cd90db1d0f55d3e8ff524594c193f
broskj <kjbrost@gmail.com>
2015-02-16 21:15:04 -0500

    bug fix with edittext not scrolling

--- app/src/main/java/com/jacksonvillecomedy/broskj/jaxcomedy/Contact.java
+++ app/src/main/java/com/jacksonvillecomedy/broskj/jaxcomedy/Contact.java
@@ -54,7 +54,6 @@ public class Contact extends Activity {
         makePhoneNumberClickable();
         manageActionBar();
         scaleBackground();
-
     }//end onCreate
 
     public void declarations(){
